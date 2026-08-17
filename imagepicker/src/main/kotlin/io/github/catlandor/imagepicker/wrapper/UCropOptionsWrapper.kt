d9652e55c335e26305917b525c0420392d7d106f
Vladyslav Dovhal <vladyslav.dovhal@quarks.tech>
2026-08-17 22:42:19 +0300

    Simplify UCropOptionsWrapper to use the internal option Bundle directly
    Per review feedback: instead of reflecting over UCrop.Options fields
    (which broke on static constants like EXTRA_ALLOWED_GESTURES), read
    and write the Options' own optionBundle through the Parcel. Simpler
    and avoids reflection into UCrop internals altogether.
    
    Adds an instrumented round-trip test for the Parcelable implementation.

--- imagepicker/src/main/kotlin/io/github/catlandor/imagepicker/wrapper/UCropOptionsWrapper.kt
+++ imagepicker/src/main/kotlin/io/github/catlandor/imagepicker/wrapper/UCropOptionsWrapper.kt
@@ -1,88 +1,20 @@
 package io.github.catlandor.imagepicker.wrapper
 
-import android.graphics.Bitmap
-import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import com.yalantis.ucrop.UCrop
-import java.lang.reflect.Modifier
-
-// Static/constant fields (e.g. UCrop.Options.EXTRA_ALLOWED_GESTURES) aren't part of an
-// instance's state and reflectively writing to them throws IllegalAccessException, so they
-// must be excluded from both directions of the (de)serialization below.
-private val instanceFields =
-    UCrop.Options::class.java.declaredFields
-        .filterNot { Modifier.isStatic(it.modifiers) }
 
 class UCropOptionsWrapper(val options: UCrop.Options) : Parcelable {
     constructor(parcel: Parcel) : this(
         UCrop.Options().apply {
-            // Read options from parcel dynamically
-            val fieldMap = instanceFields.associateBy { it.name }
-            fieldMap.forEach { (name, field) ->
-                field.isAccessible = true
-                try {
-                    when (field.type) {
-                        Int::class.java -> {
-                            field.setInt(this, parcel.readInt())
-                        }
-
-                        Float::class.java -> {
-                            field.setFloat(this, parcel.readFloat())
-                        }
-
-                        Boolean::class.java -> {
-                            field.setBoolean(
-                                this,
-                                parcel.readByte().toInt() != 0
-                            )
-                        }
-
-                        String::class.java -> {
-                            field.set(this, parcel.readString())
-                        }
-
-                        Bitmap.CompressFormat::class.java -> {
-                            field.set(
-                                this,
-                                parcel.readSerializable() as Bitmap.CompressFormat?
-                            )
-                        }
-
-                        Bundle::class.java -> {
-                            field.set(
-                                this,
-                                parcel.readBundle(Bundle::class.java.classLoader)
-                            )
-                        }
-                    }
-                } catch (e: IllegalArgumentException) {
-                    // Skip unsupported fields
-                } catch (e: IllegalAccessException) {
-                    // Skip fields that reflection isn't allowed to write (e.g. static constants)
-                }
-            }
+            parcel
+                .readBundle(UCrop.Options::class.java.classLoader)
+                ?.let { optionBundle.putAll(it) }
         }
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        // Write options to parcel dynamically
-        val fieldMap = instanceFields.associateBy { it.name }
-        fieldMap.forEach { (_, field) ->
-            field.isAccessible = true
-            try {
-                when (val value = field.get(this.options)) {
-                    is Int -> parcel.writeInt(value)
-                    is Float -> parcel.writeFloat(value)
-                    is Boolean -> parcel.writeByte((if (value) 1 else 0).toByte())
-                    is String -> parcel.writeString(value)
-                    is Bitmap.CompressFormat -> parcel.writeSerializable(value)
-                    is Bundle -> parcel.writeBundle(value)
-                }
-            } catch (e: IllegalArgumentException) {
-                // Skip unsupported fields
-            }
-        }
+        parcel.writeBundle(options.optionBundle)
     }
 
     override fun describeContents(): Int = 0
